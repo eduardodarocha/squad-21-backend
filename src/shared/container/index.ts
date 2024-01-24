493116591bc4a6d61ae32f811f536a857dd184ec
Bruna Lima <obrbrunalima@gmail.com>
2024-01-24 16:06:04 -0300

    :bug: fix: Deleção da pasta StorageAvatarProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,6 @@ import { container } from 'tsyringe';
 
 import '@modules/users/providers';
 
-import './providers/StorageAvatarProvider';
-
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/usersRepository';
 
--- src/shared/container/providers/StorageAvatarProvider/models/IStorageAvatarProvider.ts
+++
@@ -1,4 +0,0 @@
-export default interface IStorageAvatarProvider {
-  saveFile(file: string): Promise<string>;
-  deleteFile(file: string): Promise<void>;
-}
