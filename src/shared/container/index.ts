eb965c7cf1baa7745692d7231c62d7d7cb62972b
Eduardo Rocha <eduardomfrocha@gmail.com>
2024-01-25 14:13:18 -0300

    :construction: import repositories

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,13 @@
 import { container } from 'tsyringe';
 
 import '@modules/users/providers';
+import '@modules/projects/providers';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/usersRepository';
+
+import IProjectRepository from '@modules/projects/repositories/IProjectsRepository';
+import ProjectsRepository from '@modules/projects/infra/typeorm/repositories/projectsRepository';
 
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/userTokensRepository';
@@ -16,4 +20,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
+);
+
+container.registerSingleton<IProjectRepository>(
+  'ProjectsRepository',
+  ProjectsRepository,
 );
