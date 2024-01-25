f794db407315db62e149958dd1eff02720affa13
Eduardo Rocha <eduardomfrocha@gmail.com>
2024-01-25 17:47:20 -0300

    :bug: add project route - index.ts

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,9 +2,14 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions/sessions.routes';
 
+import projectsRouter from '@modules/projects/infra/http/routes/projects/projects.routes';
+// import sessionsRouter from '@modules/projects/infra/http/routes/sessions/sessions.routes';
+
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+
+routes.use('/projects', projectsRouter);
 
 export default routes;
